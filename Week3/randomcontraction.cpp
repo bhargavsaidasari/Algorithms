5057672a5c1c29f5425825e75aa9947f19e3ee3a
neophyte226 <sdasari@ncsu.edu>
2017-10-16 22:14:04 -0400

    Karger's Algorithm-Bugs Fixed

--- Week3/randomcontraction.cpp
+++ Week3/randomcontraction.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<vector>
 #include<cstdlib>
+#include<iterator>
+#include<algorithm>
+#include<stdlib.h>
+#include<ctime>
 using namespace std;
 
 struct node
@@ -48,14 +52,40 @@ class Graph
 		
 		node *newnode=new node;
 		int edge;
+		newnode=list[dest].head;
 		if(list[dest].head != NULL && list[src].head != NULL){
 		
 			vector<int> src_elements=Elements(src);
-			vector<int> dest_elements=Elements(dest);
-			counter=counter-1;}
+			while(list[dest].head != NULL){
+			
+			int x=newnode->data;
+
+			newnode=newnode->next;
+			list[dest].head=newnode;
+			//if(std::find(src_elements.begin(), src_elements.end(), x) == src_elements.end()) 
+				addEdge(src,x,0);
+				replaceEdge(x,dest,src);
+				}
+		counter=counter-1;
+		return(counter);}
 		else return(counter);
 		
 		}
+	
+	void replaceEdge(int src,int replaced,int replace){
+		node *newnode1 =new node;
+		newnode1=list[src].head;
+		while(newnode1 != NULL){
+			int test=newnode1->data;
+			if(test==replaced){
+				newnode1->data=replace;
+				
+				}
+			newnode1=newnode1->next;
+			
+			
+			}
+}
 		
 
 	void printGraph()
@@ -76,6 +106,7 @@ class Graph
 	
 	vector<int> Elements(int src){
 		vector<int> element;
+		element.push_back(src);
 		node *newnode=new node;
 		newnode=list[src].head;
 		while(newnode != NULL){
@@ -95,18 +126,24 @@ class Graph
 
 int main(){
 
-	Graph gh(6);
+	Graph gh(4);
 	gh.addEdge(0,1,1);
 	gh.addEdge(0,2,1);
 	gh.addEdge(0,3,1);
-	gh.addEdge(2,3,1);
 	gh.addEdge(1,3,1);
-	gh.addEdge(1,4,1);
-	gh.addEdge(1,5,0);	
+	gh.addEdge(1,3,1);
+	gh.addEdge(2,3,1);	
 	gh.printGraph();
 	int count=4;
-
-		count=gh.edgeContraction(0,1,count);
+	int v1,v2;
+	srand(time(NULL));
+        while(count!=2){
+	v1=rand()%4;
+	v2=rand()%4;
+	cout<<v1<<v2<<endl;
+	if(v1 !=v2){
+	count=gh.edgeContraction(v1,v2,count);}}
+	
 		
 			
         gh.printGraph();
